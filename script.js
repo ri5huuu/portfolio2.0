7aa6b6267198670037980090918c1905f64709f4
Ravi Prakash <raviprakashdbg21@gmail.com>
2025-07-30 19:04:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let Mode = document.querySelector("body");
 let headings = document.querySelectorAll("h2, h3, h4, i, h5, p");
 let resume = document.querySelector(".resume button");
 let nav = document.querySelector(".navbar");
+let back =document.querySelector("html,body");
 
 let value = 0;
 
@@ -13,6 +14,7 @@ Button.addEventListener("click", function () {
         Button.style.color = "teal";
         nav.style.color = "teal";
         resume.style.backgroundColor="#b5b738";
+        back.style.backgroundColor="white"
         headings.forEach(h => h.style.color = "teal");
         value = 1;
     } else {
@@ -21,6 +23,7 @@ Button.addEventListener("click", function () {
         Button.style.color = "white";
         nav.style.color = "white";
         resume.style.backgroundColor="grey";
+        back.style.backgroundColor="black"
         headings.forEach(h => h.style.color = "white");
         value = 0;
     }
